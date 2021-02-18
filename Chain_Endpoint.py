a591db29df090dd3defa3f38d4baf2216140a73d
Bill Wright <bill@wwwright.com>
2021-02-18 07:12:39 -0700

    Added the cain_length property to Chain_Endpoint objects

--- Chain_Endpoint.py
+++ Chain_Endpoint.py
@@ -4,18 +4,22 @@ from grid_utils import tuple_cross
 
 class Chain_Endpoint(object):
 
-    def __init__(self, endpoint_cell, open_neighbors, required_neighbor_value, value_difference_to_another_chain):
+    def __init__(self, endpoint_cell, open_neighbors, required_neighbor_value, value_difference_to_another_chain, chain_length):
         self.endpoint = endpoint_cell
         self.open_neighbors = open_neighbors
         self.required_neighbor_values = required_neighbor_value
         self.value_difference_to_another_chain = value_difference_to_another_chain
+        self.chain_length = chain_length
 
     def __len__(self):
         return len(self.open_neighbors)
 
     def __repr__(self):
-        return f"Chain Endpoint({self.endpoint}); open neighbors={self.open_neighbors}, " + \
-               f"required neighbor values= {self.required_neighbor_values}, distance={self.value_difference_to_another_chain}"
+        return f"Chain({self.endpoint}); " + \
+               f"distance={self.value_difference_to_another_chain}, " + \
+               f"len:{self.chain_length}, " + \
+               f"required neighbor values= {self.required_neighbor_values}, " + \
+               f"open neighbors={self.open_neighbors}"
 
     def __eq__(self, other):
         return self.endpoint.address == other.endpoint.address and \
